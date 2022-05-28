5267498574741c3c7a05956798ee4dc37da7c945
Lagarttixa <abner.cars@hotmail.com>
2022-05-27 22:04:29 -0300

    Aula 27/05

--- App.js
+++ App.js
@@ -1,47 +1,132 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState} from 'react';
+import { ActivityIndicator, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View, Button, Alert } from "react-native"; 
 
-import estilo from "./Estilo";
-import zoeira from "./assets/gratisography-10-free-stock-photo-800x525.jpg";
+export default function App (){
+  const [ cep, alterarCep ] = React.useState("00000-000");
+  const [ logadouro, alterarLogagouro ] = React.useState("");
+  const [ complemento, alterarComplemento ] = React.useState("");
+  const [ bairro, alterarBairro ] = React.useState("");
+  const [ localidade, alterarLocalidade ] = React.useState("");
+  const [ uf, alterarUf ] = React.useState("");
+  const [ ibge, alterarIbge ] = React.useState("");
+  const [ gia, alterarGia ] = React.useState("");
+  const [ ddd, alterarDDD ] = React.useState("");
+  const [ siafi, alterarSiafi ] = React.useState("");
 
-const App = (props) => {
-  const[estado, setEstado] = useState('Texto Aleatorio');
+  const [ isLoading, setLoading ] = React.useState(false);
 
-  const mensagem = () =>{
-    alert(estado)
+  const buscarEndereco = async () => {
+    try{
+      const response = await fetch ('https://viacep.com.br/ws/' + cep.toString().replace("-","") + '/json/');
+      const json = await response.json();
+      alterarLogagouro(json.logadouro);
+      alterarComplemento(json.complemento);
+      alterarBairro(json.bairro);
+      alterarLocalidade(json.localidade);
+      alterarUf(json.uf);
+      alterarIbge(json.ibge);
+      alterarGia(json.gia);
+      alterarDDD(json.ddd);
+      alterarSiafi(json.siafi);
+    } catch (error){
+      Alert.alert(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
-  useEffect(() =>{
-    mensagem()
-  }, [estado]);
-
+  useEffect(() => {
+    buscarEndereco();
+  }, []);
+  
   return (
-    <View style={estilo.container}>
-      <Text style={estilo.texto}>hola!</Text>
-      <TextInput
-        style={estilo.caixa}
-      />
-      <Button title="click here" />
-      <TouchableOpacity onPress={(value) => {
-          setEstado(value);
-        }}>
-        <Text> Trazer imagem aqui</Text>
-        <Image
-          source={zoeira}
-          alt="Imagem da zoeira"
-          style={estilo.botaoImagem}
-        />
-      </TouchableOpacity>
-    </View>
+    <ScrollView>
+      <SafeAreaView style = {style.conteiner}>
+        <View>
+          <Text style = { StyleSheet.texto }>
+            Entre com o CEP para pesquisar: 00000-000
+          </Text>
+          <TextInput 
+            style = { StyleSheet.input }
+            onChangeText = { alterarCep }
+            value = { cep }
+            keyboardType = "numeric"
+            maxLength = {9}
+            onFocus = {()=>alterarCep("")}
+          />
+          {isLoading ? <ActivityIndicator/> :
+          <Button style = { style.botao }
+            onPress = {() => {
+              setLoading(true);
+              buscarEndereco();
+            }
+          }
+            title = "Consumindo ViaCEP API"
+            color = "#841584"
+            accessibilityLabel="Aprenda mais sobre consumo de API REST"
+          />}
+          <TextInput
+            style = {style.inputLange}
+            onChangeText = {alterarLogadouro}
+            value = {logadouro}
+            keyboardType = "default"
+            hintText = {"Logadouro"}
+            multiline = {true}
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarComplemento}
+            value = {complemento}
+            keyboardType = "default"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarBairro}
+            value = {bairro}
+            keyboardType = "default"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarLocalidade}
+            value = {localidade}
+            keyboardType = "default"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarUf}
+            value = {uf}
+            keyboardType = "default"
+            maxLength={2}
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarIbge}
+            value = {ibge}
+            keyboardType = "numeric"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarGia}
+            value = {gia}
+            keyboardType = "numeric"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarDDD}
+            value = {ddd}
+            keyboardType = "numeric"
+          />
+          <TextInput
+            style = {style.input}
+            onChangeText = {alterarSiafi}
+            value = {siafi}
+            keyboardType = "numeric"
+          />
+          <StatusBar style = "auto"/>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
-};
 
-export default App;
+}
---
+++ style.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        margin: 16,
+        marginTop: 48
+    },
+    texto: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    input: {
+        height: 40,
+        margin: 3,
+        borderWidth: 1,
+        padding: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignSelf: "center",
+        maxWidth: 256
+    },
+    inputLarge: {
+        height: 40,
+        margin: 3,
+        borderWidth: 1,
+        padding: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignSelf: "center"
+    },
+    botao: {
+        height: 40,
+        margin: 3,
+        borderWidth: 1,
+        padding: 1,
+        width: 50,
+        borderRadius: 16
+    },
+    
+        });
+
+export default estilo;
